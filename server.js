bf7fa7b60df6ca4f979ea682858fa389d2803b85
rafifiaan <rafialiefian03@gmail.com>
2024-11-05 22:33:10 +0700

    perf: test new config v19

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
 
 app.use((req, res, next) => {
     console.log(`Request received: ${req.method} ${req.url}`);
