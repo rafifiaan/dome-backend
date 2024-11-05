70c6671d2878d115a5778720e7c420776519c9a0
rafifiaan <rafialiefian03@gmail.com>
2024-11-05 22:19:25 +0700

    perf: test new config v15

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
 // Log untuk memeriksa apakah drizzle-orm terpasang dengan benar
 app.listen(port, () => {
     console.log(`Yeay! Server is successfully running on port: ${port}`);
