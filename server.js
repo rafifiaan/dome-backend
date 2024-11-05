62fae6734f56099e8fbddb34cfb8f2692c9e7688
rafifiaan <rafialiefian03@gmail.com>
2024-11-05 21:48:12 +0700

    perf: test new config v8

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
